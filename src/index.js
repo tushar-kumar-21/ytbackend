b31dcf23c941baa6e10f3fc9ad0972bf2a822027
tushar <tushar.github21@gmail.com>
2024-03-23 23:42:03 +0530

    fc

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// require('dotenv').config({path:'./env'})
+
+import express from 'express';
+import connectDB from './db/index.js';
+import dotenv from "dotenv";
+import { app } from './app.js';
+
+dotenv.config({
+    path: './env'
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+
+connectDB().then().catch((err) => {
+    console.log("MONGO db connection faild !!!!", err);
+})
