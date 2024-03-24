013976b69b6256e4c798b6d6f143fe8310e36789
tushar <tushar.github21@gmail.com>
2024-03-24 16:03:39 +0530

    fix

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,7 +13,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: 'auto'
     })
-    fs.unlinkSync(localFilePath)
+    // fs.unlinkSync(localFilePath)
     return response;
   } catch (err) {
     fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation goes failed;
