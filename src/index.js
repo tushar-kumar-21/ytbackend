d7c2124ca133a896135e2c5e0859410f309124e0
tushar <tushar.github21@gmail.com>
2024-03-24 15:50:12 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // require('dotenv').config({path:'./env'})
-import { connectDB } from './db/index.js'
+import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import { app } from './app.js';
 
@@ -12,6 +12,16 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log('MONGODB connection error', error);
+        process.exit(1)
+    }
+}
 
 connectDB().then().catch((err) => {
     console.log("MONGO db connection faild !!!!", err);
