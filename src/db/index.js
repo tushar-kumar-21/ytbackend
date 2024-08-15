ba5d6c499140d8a433e289b76fee723079822f07
tushar <tushar.github21@gmail.com>
2024-08-15 15:57:40 +0530

    ecosytem file

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://tushargithub21:tk4tushar@cluster0.cpgk2ez.mongodb.net/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log('MONGODB connection error', error);
